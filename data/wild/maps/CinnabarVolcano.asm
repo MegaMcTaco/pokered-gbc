c8345a52ed3e758b863392933235a55ae9bb121b
MegaMcTaco <blaze3777@hotmail.com>
2025-12-10 12:12:22 +1100

    Volcano Mon Updates

--- data/wild/maps/CinnabarVolcano.asm
+++ data/wild/maps/CinnabarVolcano.asm
@@ -1,15 +1,16 @@
 CinnabarVolcanoWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 40, PONYTA ; 51
-	db 40, GROWLITHE ; 51
-	db 40, CHARMELEON ; 39
-	db 43, VULPIX ; 25
-	db 45, RAPIDASH ; 13
-	db 45, NINETALES ; 10
+	db 15, CHARMANDER 
+	db 30, PONYTA ; 51
+	db 32, GROWLITHE ; 51
+	db 30, CHARMELEON ; 39
+	db 32, VULPIX ; 25
+	db 42, RAPIDASH ; 13
+;	db 42, NINETALES ; 10
 	db 45, ARCANINE ; 10
 	db 44, MAGMAR ;  3
 	db 46, RHYDON ;  1
-	db 48, CHARIZARD ;  1
+	db 45, CHARIZARD ;  1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate

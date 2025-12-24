aa3b8451aa177d7d94aeab9dd20c7500fe533525
MegaMcTaco <blaze3777@hotmail.com>
2025-12-24 14:42:11 +1100

    Type changes
    Gyarados now Water/Dragon and Golduck now Water/Psychic

--- data/pokemon/base_stats/golduck.asm
+++ data/pokemon/base_stats/golduck.asm
@@ -3,7 +3,7 @@
 	db  80,  82,  78,  85,  80
 	;   hp  atk  def  spd  spc
 
-	db WATER, WATER ; type
+	db WATER, PSYCHIC_TYPE ; type
 	db 75 ; catch rate
 	db 174 ; base exp
 
@@ -21,9 +21,9 @@ ENDC
 	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
 	     DOUBLE_EDGE,  BUBBLEBEAM,   WATER_GUN,    ICE_BEAM,     BLIZZARD,     \
 	     HYPER_BEAM,   PAY_DAY,      SUBMISSION,   COUNTER,      SEISMIC_TOSS, \
-	     RAGE,         DIG,          MIMIC,        DOUBLE_TEAM,  BIDE,         \
-	     SWIFT,        SKULL_BASH,   REST,         SUBSTITUTE,   SURF,         \
-	     STRENGTH
+	     RAGE,         DIG,          PSYCHIC_M,	   MIMIC,        DOUBLE_TEAM,  \
+	     BIDE, 		   SWIFT,        SKULL_BASH,   REST,         SUBSTITUTE,   \
+	     SURF, 		   STRENGTH
 	; end
 
 	db BANK(GolduckPicFront)
